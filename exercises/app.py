2a2a63acdc5cb0f21a942fffb75584f039c59979
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:19:18 +0300

    oi

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
-    pass
+	list1 = ["or","orr","orrr"]
+	{% for i in list1 %}
+	{% endfor %}
+	return render_template("index.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
