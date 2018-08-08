a3ae12781cfba6f9fbc3aca735299f8b5e0dbf25
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:07:50 +0300

    oi oi itz me

--- exercises/app.py
+++ exercises/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
 	fav_players = ["player1","player2","player3","player45"]
-	return render_template("index.html",likes_same_sports=False,list1=list1)
+	return render_template("index.html",likes_same_sports=False,list1=fav_players)
 
 if __name__ == '__main__':
    app.run(debug = True)
